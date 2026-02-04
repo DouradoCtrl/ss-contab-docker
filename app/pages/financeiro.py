82eb3aa734586fe43af69e8ee8864c62327e6bc3
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-04 10:10:29 -0300

    add: new abas

--- app/pages/financeiro.py
+++ app/pages/financeiro.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title("Financeiro")
 st.write("Gerencie entradas, saídas e gastos fixos.")
+
+tab_entrada, tab_saida, tab_gastos_fixos = st.tabs([
+    ":material/list_alt: Entradas", 
+    ":material/event: Despesas", 
+    ":material/assignment_late: Gastos Fixos    "
+])
