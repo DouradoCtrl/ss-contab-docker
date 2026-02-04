6ab4826c0643fc851515a7793bfebbdec86acea8
DouradoCtrl <gdouradoestudante@gmail.com>
2026-02-04 01:22:37 -0300

    add: show datas clients

--- app/pages/clientes.py
+++ app/pages/clientes.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
+conn = st.connection("postgresql", type="sql")
+df = conn.query('SELECT * FROM clientes;', ttl="10m")
+
 st.title("Clientes")
 st.write("Veja, cadastre, edite e exclua clientes.")
+
+st.dataframe(df)
